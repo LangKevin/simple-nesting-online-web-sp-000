2e502f22b4eec7f3646c2cbb919a056b1e012de1
kevin lang <langkevina@gmail.com>
2019-06-01 13:50:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,21 +130,21 @@ def adding_to_dennis
         }
      }
 # ["C", "Assembly"]
-		 programmer_hash["dennis_ritchie"][:languages] << ["C", "Assembly"]
+		 programmer_hash[:dennis_ritchie][:languages] << ["C", "Assembly"]
 		 programmer_hash
 end
-def test
-  contacts = {
-    "Jon Snow" => {
-      name: "Jon",
-      email: "jon_snow@thewall.we",
-      favorite_ice_cream_flavors: ["chocolate", "vanilla"]  },
-    "Freddy Mercury" => {
-      name: "Freddy",
-      email: "freddy@mercury.com",
-      favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-	  }
-  }
-	contacts["Jon Snow"][:favorite_ice_cream_flavors] << "mint chip"
-	puts contacts
-end
+# def test
+#   contacts = {
+#     "Jon Snow" => {
+#       name: "Jon",
+#       email: "jon_snow@thewall.we",
+#       favorite_ice_cream_flavors: ["chocolate", "vanilla"]  },
+#     "Freddy Mercury" => {
+#       name: "Freddy",
+#       email: "freddy@mercury.com",
+#       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+# 	  }
+#   }
+# 	contacts["Jon Snow"][:favorite_ice_cream_flavors] << "mint chip"
+# 	puts contacts
+# end
