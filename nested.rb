b73c55dc2ebee3472266cc764d82d84a7e7a9402
kevin lang <langkevina@gmail.com>
2019-06-01 13:49:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -141,7 +141,7 @@ def test
       favorite_ice_cream_flavors: ["chocolate", "vanilla"]  },
     "Freddy Mercury" => {
       name: "Freddy",
-      email: "freddy@mercury.com", 
+      email: "freddy@mercury.com",
       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
 	  }
   }
